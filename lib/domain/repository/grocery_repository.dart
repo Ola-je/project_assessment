9f2ebc75f6d85d9d445f088515aada34da7b53a4
ola-je <kidanejerry523@gmail.com>
2024-08-27 10:58:04 +0300

    Format code

--- lib/domain/repository/grocery_repository.dart
+++ lib/domain/repository/grocery_repository.dart
@@ -1,6 +1,6 @@
 import 'package:project_assessment/domain/entity/grocery_entity.dart';
 
 abstract class GroceryRepository{
-   Future<List<GroceryEntity>> getGroceries();
+  Future<List<GroceryEntity>> getGroceries();
   Future<GroceryEntity> getGroceryById(String id);
 }
