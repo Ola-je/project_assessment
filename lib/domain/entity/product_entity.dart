0df3dc179814bd64ec17b06360e21e886cf76444
ola-je <kidanejerry523@gmail.com>
2024-08-27 09:50:14 +0300

    ✨ Initialize Entity

---
+++ lib/domain/entity/product_entity.dart
@@ -0,0 +1,56 @@
+class Product {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final double rating;
+  final double price;
+  final double discount;
+  final String description;
+  final List<Option> options;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.rating,
+    required this.price,
+    required this.discount,
+    required this.description,
+    required this.options,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      imageUrl: json['imageUrl'],
+      rating: json['rating'].toDouble(),
+      price: json['price'].toDouble(),
+      discount: json['discount'].toDouble(),
+      description: json['description'],
+      options: (json['options'] as List<dynamic>)
+          .map((option) => Option.fromJson(option))
+          .toList(),
+    );
+  }
+}
+
+class Option {
+  final String id;
+  final String name;
+  final double price;
+
+  Option({
+    required this.id,
+    required this.name,
+    required this.price,
+  });
+
+  factory Option.fromJson(Map<String, dynamic> json) {
+    return Option(
+      id: json['id'],
+      name: json['name'],
+      price: json['price'].toDouble(),
+    );
+  }
+}
